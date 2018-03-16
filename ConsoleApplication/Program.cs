21c282ea4e51ddb72988a3acc7345aeb57dc9869
dplayerd <dplayerd@gmail.com>
2018-03-16 23:02:28 +0800

    ConsoleApplication Init

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemplateFramework.Global.init();
+            TemplateFramework.Global.execute();
+
+
+            Console.WriteLine("Execute completed. Please press ENTER.");
+            Console.ReadLine();
+        }
+    }
+}
